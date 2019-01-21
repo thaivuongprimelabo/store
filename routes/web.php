f4e1995427b270a237eacb2c21060879331af28a
thaivuong.primelabo <thai.vuong@primelabo.com.vn>
2019-01-21 17:20:56 +0700

    Store

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Support\Facades\Auth;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::group(['prefix' => 'auth', 'namespace' => 'Auth'], function () {
+    
+    // Authentication Routes...
+    $this->get('/login', 'LoginController@showLoginForm')->name('login');
+    $this->post('/login', 'LoginController@login');
+    $this->post('/logout', 'LoginController@logout')->name('logout');
+    $this->get('/dashboard', 'DashboardController@index')->name('dashboard');
+    // Registration Routes...
+//     $this->get('/register', 'RegisterController@showRegistrationForm')->name('register');
+//     $this->post('/register', 'RegisterController@register');
+    
+    // Password Reset Routes...
+//     $this->get('/password/reset', 'ForgotPasswordController@showLinkRequestForm')->name('password.request');
+//     $this->post('/password/email', 'ForgotPasswordController@sendResetLinkEmail')->name('password.email');
+//     $this->get('/password/reset/{token}', 'ResetPasswordController@showResetForm')->name('password.reset');
+//     $this->post('/password/reset', 'ResetPasswordController@reset');
+    
+});
+
+
+
